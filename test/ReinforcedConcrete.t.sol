331a71d74ee5422de4433bc49b89a4316e101e84
rymnc <43716372+rymnc@users.noreply.github.com>
2023-10-26 02:28:44 +0530

    feat: bricks macro 🧱

--- test/ReinforcedConcrete.t.sol
+++ test/ReinforcedConcrete.t.sol
@@ -17,7 +17,10 @@ contract RCTest is Test {
             0x0bc1f4af0cd701956511c0e5d4927d17ff43c6ea675de7c1dff3f99132584040,
             0x2d9397c680616ebc94f9c5bbba010835ec72d65286ae8ae184cc39daffee77c4
         );
-        assertEq(val, 0x05a3f4f1741752744392ce84bb565315b13bb9a73758412f799276333019d901);
+        // concrete assertion
+        // assertEq(val, 0x05a3f4f1741752744392ce84bb565315b13bb9a73758412f799276333019d901);
+        // bricks assertion
+        assertEq(val, 0x21b2d62d0463a46cd3ee90f6e6f02fadfa0ef96239e05d925cf21c11950be875);
     }
 
     function testHasherGas() public view {
