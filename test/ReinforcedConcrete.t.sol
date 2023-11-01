bf8b8b831e8e86c7c0b87ff6e057226292dd6e09
rymnc <43716372+rymnc@users.noreply.github.com>
2023-11-01 16:27:11 +0530

    feat: reinforced concrete finished. gas usage is terrible

--- test/ReinforcedConcrete.t.sol
+++ test/ReinforcedConcrete.t.sol
@@ -28,7 +28,15 @@ contract RCTest is Test {
         // decompose and sbox assertion (26) sbox[571] = 250
         // assertEq(val, 250);
         // decompose and sbox assertion final
-        assertEq(val, 306);
+        // assertEq(val, 306);
+        // compose assertion (s0_composed)
+        // assertEq(val, 0x160d7fd3dafaf85a54ccaa0640b5fb25287ac10fbd40d1bf1900f39914817815);
+        // compose assertion (s1_composed)
+        // assertEq(val, 0x1fcc69668b4af2713825b3fd7286a07ae9beb91b6b3079ca29de1628e72fb35f);
+        // compose assertion (s2_composed)
+        // assertEq(val, 0x0a2c0a0e21b7de350008dfa9705f2cccd88464893d348ad068689d1a70d9e62f);
+        // final rounds assertion
+        assertEq(val, 0x2d4ae429dbdd40600828c21ee52c2ec9ba11cb965effaa441dd62a74e249f740);
     }
 
     function testHasherGas() public view {
