7b34cac0c9b346f9594a2f8d5cc4faf0d47997d9
rymnc <43716372+rymnc@users.noreply.github.com>
2024-06-26 16:42:25 +0530

    fix: check if inputs are within ff

--- test/ReinforcedConcrete.t.sol
+++ test/ReinforcedConcrete.t.sol
@@ -17,25 +17,6 @@ contract RCTest is Test {
             0x0bc1f4af0cd701956511c0e5d4927d17ff43c6ea675de7c1dff3f99132584040,
             0x2d9397c680616ebc94f9c5bbba010835ec72d65286ae8ae184cc39daffee77c4
         );
-        // concrete assertion
-        // assertEq(val, 0x05a3f4f1741752744392ce84bb565315b13bb9a73758412f799276333019d901);
-        // bricks assertion
-        // assertEq(val, 0x21b2d62d0463a46cd3ee90f6e6f02fadfa0ef96239e05d925cf21c11950be875);
-        // first rounds assertion
-        // assertEq(val, 0x0a1bbc638e16abd280864ccd79f173586743ae47a13140600d3cf98a8ffa4dd4);
-        // long div assertion (quotient 26)
-        // assertEq(val, 140);
-        // decompose and sbox assertion (26) sbox[571] = 250
-        // assertEq(val, 250);
-        // decompose and sbox assertion final
-        // assertEq(val, 306);
-        // compose assertion (s0_composed)
-        // assertEq(val, 0x160d7fd3dafaf85a54ccaa0640b5fb25287ac10fbd40d1bf1900f39914817815);
-        // compose assertion (s1_composed)
-        // assertEq(val, 0x1fcc69668b4af2713825b3fd7286a07ae9beb91b6b3079ca29de1628e72fb35f);
-        // compose assertion (s2_composed)
-        // assertEq(val, 0x0a2c0a0e21b7de350008dfa9705f2cccd88464893d348ad068689d1a70d9e62f);
-        // final rounds assertion
         assertEq(val, 0x2d4ae429dbdd40600828c21ee52c2ec9ba11cb965effaa441dd62a74e249f740);
     }
 
@@ -44,5 +25,10 @@ contract RCTest is Test {
             0x0bc1f4af0cd701956511c0e5d4927d17ff43c6ea675de7c1dff3f99132584040,
             0x2d9397c680616ebc94f9c5bbba010835ec72d65286ae8ae184cc39daffee77c4
         );
+    }
+
+    function testRevert() public {
+        vm.expectRevert();
+        rc.hash(0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000002, 0);
     }
 }
